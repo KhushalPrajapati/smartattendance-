32f8a1a7b7f6ded437b3fe42afe5f1f130273abf
Khushal Prajapati <khushal45y@gmail.com>
2025-08-27 15:45:34 +0530

    Add files via upload

---
+++ myattendance/src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="px-4">
+      <h2 className="text-2xl font-bold">Welcome to Smart Attendance Planner</h2>
+      <p className="mt-2">Track and manage your attendance easily.</p>
+    </div>
+  );
+}
+export default Home;
---
+++ myattendance/src/pages/Profile.js
@@ -0,0 +1,8 @@
+ function Profile() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold">Profile Page</h2>
+    </div>
+  );
+}
+export default Profile;
